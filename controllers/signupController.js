1a71a8f6ace38ce7d0ebf0f2310ce91339434c34
WendyDarli <139488528+WendyDarli@users.noreply.github.com>
2024-06-12 19:45:30 -0300

    Sign Up controller
    - Display the signup form to the user
    - Receive and validate the submitted form data
    - Return validation errors if any, or add a new user to the database if the data is valid

---
+++ controllers/signupController.js
@@ -0,0 +1,119 @@
+const asyncHandler = require('express-async-handler');
+const { body, validationResult } = require("express-validator");
+
+const User = require('../models/users');
+
+exports.signup_get = asyncHandler(async(req, res, next) => {
+    // show the form
+    
+    const errors = validationResult(req);
+    res.render('signup', {
+        title: "Sign Up",
+        errors: errors.array()
+    });
+});
+
+exports.signup_post = [
+    
+    body('username')
+    .trim()
+    .isLength({min:3}).withMessage('Username must contain more than 3 characteres.')
+    .matches(/^[a-zA-Z ]*$/).withMessage('Username must contain only letters and spaces.')
+    .custom(async value => {
+        const username = await User.findUserByUsername(value);
+        if(username) {
+            throw new Error('Username already in use.')
+        }
+    })
+    .escape(),
+    
+    body('fname')
+    .trim()
+    .isLength({min:3}).withMessage('First name must contain more than 3 characteres.')
+    .matches(/^[a-zA-Z ]*$/).withMessage('First name must contain only letters and spaces.')
+    .escape(),
+    
+    body('lname')
+    .trim()
+    .isLength({min:3}).withMessage('Last name must contain more than 3 characteres.')
+    .matches(/^[a-zA-Z ]*$/).withMessage('Last name must contain only letters and spaces.')
+    .escape(),
+
+    body('email')
+    .trim()
+    .isEmail().withMessage('Please use a valid E-mail address.')
+    .custom(async value => {
+        const email  = await User.findUserByEmail(value);
+        if(email) {
+            throw new Error('E-mail already in use.')
+        }
+    })
+    .escape(),
+
+    body('password')
+    .trim()
+    .isLength({min:6}).withMessage('Password must contain more than 6 characteres.')
+    .custom(value => { 
+        if(!/[A-Z]/.test(value)) {
+            throw new Error('Password must contain at least one uppercase letter.')
+        }
+        return true;
+    })
+    .custom(value => { 
+        if(!/[0-9]/.test(value)) {
+            throw new Error('Password must contain at least one number.')
+        }
+        return true;
+    })
+    .custom(value => { 
+        if(!/[!@#\$%\^\&*\)\(+=._-]/.test(value)) {
+            throw new Error('Password must contain at least one symbol.')
+        }
+        return true;
+    })
+    .escape(),
+
+    body('confirmpass')
+    .trim()
+    .custom((value, { req }) => {
+        if (value !== req.body.password) {
+            throw new Error('Passwords must match.');
+        }
+        return true; 
+    })
+    .escape(),
+
+    asyncHandler(async(req, res, next) => {
+        const errors = validationResult(req);
+
+        const user = new User({
+            username: req.body.username,
+            firstName: req.body.fname,
+            lastName: req.body.lname,
+            email: req.body.email,
+            password: req.body.password,
+
+        });
+
+        if (!errors.isEmpty()) {
+           
+
+            res.render('signup', {
+                title: 'Sign Up',
+                username: req.body.username,
+                firstName: req.body.fname,
+                lastName: req.body.lname,
+                email: req.body.email,
+                password: req.body.password,
+                confirmpass: req.body.confirmpass,
+                errors: errors.array()
+            })
+            console.log(errors)
+            
+        } else {
+            await user.save()
+            res.redirect(user.url);
+            console.log('saved');
+        }
+    })
+];
